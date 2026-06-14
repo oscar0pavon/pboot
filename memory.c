13a28de78e27d6b3174ed0db44f6fa8c02047522
Oscar Pavon <oscar@pavon.com.py>
2026-06-14 15:49:07 +0000

    memory code refactored

--- memory.c
+++ memory.c
@@ -30,6 +30,41 @@ void allocate_memory(uint64_t size, void** memory){
 
 u64 get_memory_map_key(){
 
+	struct MemoryDescriptor *mmap;
+	u64 mmap_size = 4096;
+	u64 mmap_key;
+	u64 desc_size;
+	uint32_t desc_version;
+
+
+	SystemTable* system_table = get_system_table();
+	
+	Status status;
+
+	while (1) {
+    uint64_t safe_mmap_address = 0x5000000; 
+    uint64_t pages_needed = (mmap_size + 4095) / 4096;
+
+    status = system_table->boot_table->allocate_pages(
+          EFI_ALLOCATE_ADDRESS,
+          EFI_LOADER_DATA,
+          pages_needed,
+          &safe_mmap_address
+      );
+
+    mmap = (struct MemoryDescriptor*)safe_mmap_address;
+
+		status = system_table->boot_table->get_memory_map(
+			&mmap_size,
+			mmap,
+			&mmap_key,
+			&desc_size,
+			&desc_version);
+		if (status == EFI_SUCCESS){
+			break;
+		}
+	}
+	
 
 }
 
